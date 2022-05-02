e2ad96a7a16bfd570cf374d9db0538ae78866b9f
Josh Brown <joshbrown31@gmail.com>
2022-05-02 12:07:16 -0400

    commit

---
+++ lecture_notes/may2_2022.js
@@ -0,0 +1,15 @@
+//JS DOES SO MANY THINGS
+
+//mongo -> we'll come back to this
+//express -> node backend (basically like a flask server)
+//react -> it's basically node but it displays in your browser
+//node -> js used outside of the browser. it's still js, browser-specific things just don't work in node (like alert, etc)
+
+//electron -> a way where you can make react applications but it works outside of the browser (like a desktop app)
+//react native -> react as a phone app
+//mongoDB -> js as a database
+
+//js is an interpreted language (like python) (as opposed to compiling language like c#, java, c++, etc)
+//in a compiled language, the entire program gets put together from front to back before it runs, and if there's an error, it will not compile
+//in an interpreted language, it runs without checking for errors first, and then errors will just mess stuff up if they exist
+
