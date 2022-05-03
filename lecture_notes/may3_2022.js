e7b5dc02ef194e64172aa59e4a8df0d0c46401d3
Josh Brown <joshbrown31@gmail.com>
2022-05-03 12:25:55 -0400

    commit

--- lecture_notes/may3_2022.js
+++ lecture_notes/may3_2022.js
@@ -23,3 +23,38 @@
 ////do x
 ////do y
 ////do z
+
+//Class representation of Super Smash Brothers
+
+class Fighter{
+    constructor(name, weight, strength, speed, color, playerNumber){
+        //name, weight, percentage, strength, speed, color, playerNumber
+        this.name = name;
+        this.weight = weight;
+        this.strength = strength;
+        this.speed = speed;
+        this.color = color;
+        this.playerNumber = playerNumber;
+        this.percentage = 0; //rather than taking a value from the parameter, every fighter starts at 0
+    }
+
+    attack(opponent){
+        if (opponent instanceof Fighter) { //make sure the opponent is a Fighter before doing anything
+            opponent.percentage += this.strength;
+            console.log('${this.name} attacked ${opponent.name} and dealt ${this.strength} percent damage') //gotta use backticks to use js syntax in the string!
+        }
+    }
+    special(opponent){
+        if (opponent instanceof Fighter) { //make sure the opponent is a Fighter before doing anything
+            opponent.percentage += this.strength * this.weight;
+            console.log(`${this.name} attacked ${opponent.name} with a special attack and dealt ${this.strength * this.weight} percent damage`)
+        }
+    }
+}
+
+const sora = new Fighter("Sora", 3, 6, 5, "red");
+const yoshi = new Fighter("Yoshi", 5, 5, 6, "green");
+
+// console.log(sora);
+
+yoshi.special(sora);
