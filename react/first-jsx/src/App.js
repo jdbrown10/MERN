eda54d2742f0e3fa2866c8545ac556186552b0d9
Josh Brown <joshbrown31@gmail.com>
2022-05-04 11:05:10 -0400

    commit

---
+++ react/first-jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo</h1>
+        <h4>Things I need to do:</h4>
+        <ul>
+          <li>Learn react</li>
+          <li>go to bed early</li>
+          <li>exercise</li>
+          <li>write in my journal</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
