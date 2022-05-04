77fdbfc16e26d08683d815da16c15a15693aa7a5
Josh Brown <joshbrown31@gmail.com>
2022-05-04 10:49:12 -0400

    commit

---
+++ javascript/ninja/sensei.js
@@ -0,0 +1,24 @@
+const Ninja = require("./ninja") //importing the Fighter class
+
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        this.drinkSake();
+        console.log("You can't step in the same river twice.")
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
