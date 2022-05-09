57977b8fbb44b354d6fbbaec37b6965f37bffeca
Josh Brown <joshbrown31@gmail.com>
2022-05-09 18:09:46 -0400

    commit all

---
+++ react/more-forms/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HookForm from './components/HookForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hook Form</h1>
+      <HookForm/>
+    </div>
+  );
+}
+
+export default App;
